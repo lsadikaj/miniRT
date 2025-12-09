a38a5f732bf6a15a91c28d1fc009c295a6e5dc6d
jimpa <jimmyparcero@protonmail.com>
2025-12-09 20:45:59 +0100

    tpf corigé

--- srcs/parser/check_args.c
+++ srcs/parser/check_args.c
@@ -1,22 +1,32 @@
 #include "../../includes/minirt.h"
 
 // a ameliorer avec un switch case peut etre
-
+void init_checkers(t_parser *checkers)
+{
+    checkers[0] = (t_parser){"A", &is_valid_ambiant};
+    checkers[1] = (t_parser){"C", &is_valid_camera};
+    checkers[2] = (t_parser){"L", &is_valid_light};
+    checkers[3] = (t_parser){"sp", &is_valid_sphere};
+    checkers[4] = (t_parser){"pl", &is_valid_plane};
+    checkers[5] = (t_parser){"cy", &is_valid_cylinder};
+    checkers[6] = (t_parser){0, NULL};
+}
 //return 0 if all args are valid, 1 if not
 int check_args(char *args)
 {
     int i = 0;
-    t_parser *checker = g_checkers;
+    t_parser checkers[7];
 
+    init_checkers(checkers);
     while(args[i])
     {
         int j = 0;
         int found = 0;
-        while(checker[j].indentifier)
+        while(checkers[j].indentifier)
         {
-            if (ft_strncmp(args, checker[j].indentifier, ft_strlen(checker[j].indentifier)) == 0)
+            if (ft_strncmp(args, checkers[j].indentifier, ft_strlen(checkers[j].indentifier)) == 0)
             {
-                if (checker[j].checker(args) == 1)
+                if (checkers[j].checker(args) == 1)
                 {
                     printf("Error\nInvalid argument line: %s", args);
                     return (1);
